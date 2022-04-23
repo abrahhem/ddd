0e81be971b3d9763cb04af4fa3d7cd4723c06801
abrahem_elnakeeb <abrahem.elnakeeb@stu.shenkar.ac.il>
2022-04-23 07:35:22 +0300

    new menu bar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
+window.onload = () => {
 
+	document.getElementById("options").onclick = () => {
+		document.getElementById("refresh").className = "fa-solid fa-arrows-rotate fa-xl fa-shake";
+	}
+	
+	document.getElementById("refresh").onclick = () => {
+		for (let index = 0; index < op.length; index++) {
+			if (op[index].selected && index == 0) {
+				chart.className = "charts-css line show-data-axes show-4-secondary-axes show-labels";
+			}
+			if (op[index].selected && index == 1) {
+				chart.className = "charts-css area show-data-axes show-4-secondary-axes show-labels";
+			}
+			if (op[index].selected && index == 2) {
+				chart.className = "charts-css column show-data-axes show-4-secondary-axes show-labels";
+			}
+		}
+		document.getElementById("refresh").className = "";
+	}
 
+	document.getElementById("open_menu").onclick = () => {
+		menu.style.left = "0%";
+	}
 
+	document.getElementById("close_menu").onclick = () => {
+		menu.style.left = "-100%";
+	}
 
+}
 
-const input = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
-
-const expand = () => {
-  searchBtn.classList.toggle("close");
-  input.classList.toggle("square");
-};
-
-searchBtn.addEventListener("click", expand);
+const menu = document.getElementById("menu__box");
+const op = document.getElementsByTagName("option");
+const chart = document.getElementById("my-chart");
+// const op = document.getElementById("options");
+let count = 0;
